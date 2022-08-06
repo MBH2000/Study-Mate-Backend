862b85bf83a3cbcf5badf89040c1110c9718b489
MBH2000 <ava201466@gmail.com>
2022-08-06 09:32:39 +0300

    45

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,6 +1,58 @@
 const express = require ('express')
 const User = require('../models/user')
 const router = new express.Router()
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    user:
+ *      type:object
+ *      required:
+ *       -   name
+ *       -  email
+ *       - password
+ *       - type
+ *      properties:
+ *          name :
+ *              type : string
+ *              description : name of the account
+ *          email :
+ *              type : string
+ *              description : email of the user
+ *          password :
+ *              type : string
+ *              description : min length 7
+ *          type :
+ *              type : string
+ *               description : store or normel user
+ *  
+ */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ */
+
 //registr router
 router.post('/user/register',async (req,res)=>{
     const user = new User(req.body)

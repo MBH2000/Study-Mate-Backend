8093f6bd64a9e62f1d30a92948d074c58704382e
MBH2000 <ava201466@gmail.com>
2022-08-18 07:58:19 +0300

    final

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017',{
+mongoose.connect('mongodb+srv://MBH:petstore2022@app.hsv9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser : true
 })
 //mongodb+srv://MBH:petstore2022@app.hsv9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
